5236c8e4ccacb3884c8cd26370ae71f720fb77d0
ValeraPrus <146940361+ValeraPrus@users.noreply.github.com>
2024-01-15 20:38:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     description='adressbook',
     url='https://github.com/Vladosinfo/knowledge_base',
-    author='Vladyslav Vyasadev, Yar Valeriy, Prus Valerii, Oleksiy Tsybrovskyi, Palamar Anna',
+    author='Pasko Vladyslav, Yaremenko Valeriy, Prus Valerii, Tsybrovskyi Oleksii, Palamar Anna',
     author_email='vladyslav.pasko@gmail.com, prusvalerii@gmail.com, tsybrovsky@gmail.com, yar.valeriy@yahoo.com, anna.palamar1210@gmail.com',
     license='BSD',
     packages=find_namespace_packages(),
